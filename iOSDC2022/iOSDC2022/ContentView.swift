632bef0937a2267fee4c85ddc85872db9380ec0e
Cookiezby <cookiezby@gmail.com>
2022-08-13 14:08:25 +0900

    feat: add main project, add json sample

---
+++ iOSDC2022/iOSDC2022/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iOSDC2022
+//
+//  Created by 朱冰一 on 2022/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOSDC2022/iOSDC2022/iOSDC2022App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDC2022App.swift
+//  iOSDC2022
+//
+//  Created by 朱冰一 on 2022/08/13.
+//
+
+import SwiftUI
+
+@main
+struct iOSDC2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
